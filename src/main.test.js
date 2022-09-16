27794d15a2f70c309c2dc6e8578aa6952ae2adc3
ransurf <ransomguest@gmail.com>
2022-09-15 21:36:03 -0700

    :sparkles: mock whole file instead of function

--- src/main.test.js
+++ src/main.test.js
@@ -1,27 +1,5 @@
 const utils = require('./utils');
 
-function sum(a, b) {
-	return a + b;
-}
-
-
-// Somewhere in your test case or test suite
-utils.getAllNotesFromFirebase = jest.fn().mockReturnValue({ notes: [note] });
-
-test("adds 1 + 2 to equal 3", () => {
-	expect(sum(1, 2)).toBe(3);
-});
-
-// create a test that fake gets from function and compares it with note
-describe("getNotes", () => {
-	it("should return notes", () => {
-		expect(utils.getAllNotesFromFirebase()).toBe([note]);
-	});
-});
-
-
-
-
 const note = {
 	_id: '1',
 	title: 'title',
@@ -30,5 +8,31 @@ const note = {
 	source: 'https://www.google.com',
 	_isDeleted: false,
 }
+
+function sum(a, b) {
+	return a + b;
+}
+
+// // Somewhere in your test case or test suite
+// utils.getAllNotesFromFirebase = jest.fn().mockReturnValue({ notes: [note] });
+
+jest.mock("./utils", () => ({
+    getAllNotesFromFirebase: jest.fn().mockReturnValue([note])
+}));
+
+test("adds 1 + 2 to equal 3", () => {
+	expect(sum(1, 2)).toBe(3);
+});
+
+// create a test that fake gets from function and compares it with note
+describe("getNotes", () => {
+	it("should return notes", () => {
+		expect(utils.getAllNotesFromFirebase()).toStrictEqual([note]);
+	});
+});
+
+
+
+
 
 
